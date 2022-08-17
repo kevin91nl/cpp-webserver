cb67993a33b0f3394e4e22cd1bf83a17f7c3df6e
kevin91nl <kevin91nl@gmail.com>
2022-08-17 10:26:45 +0200

    Updates

---
+++ README.md
@@ -0,0 +1,7 @@
+# C++ Webserver
+
+Boilerplate code for a C++ webserver.
+
+## Code Formatter
+
+Set `clang-format` as the default formatter (preferably on file save) in the editor.
--- source/main.cpp
+++ source/main.cpp
@@ -1,19 +1,17 @@
 #include "crow.h"
 
-int main()
-{
-    crow::SimpleApp app;
+int main() {
+  crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")
-    ([]()
-     { return "Hello world"; });
+  CROW_ROUTE(app, "/")
+  ([]() { return "Hello world"; });
 
-    CROW_ROUTE(app, "/json")
-    ([]
-     {
+  CROW_ROUTE(app, "/json")
+  ([] {
     crow::json::wvalue x({{"message", "Hello, World!"}});
     x["message2"] = "Hello, World.. Again!";
-    return x; });
+    return x;
+  });
 
-    app.port(18080).run();
+  app.port(18080).multithreaded().run();
 }
